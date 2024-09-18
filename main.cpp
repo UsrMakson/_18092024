4d75054248abd6e729c1348b06033e044b9a2fe0
Maksim <166782494+UsrMakson@users.noreply.github.com>
2024-09-18 12:50:14 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+using  namespace std;
+// 11
+/*
+int main() {
+    setlocale(0, "");
+    int x, y, z, k = 0;
+    cout << "x: ";
+    cin >> x;
+    cout << "y: ";
+    cin >> y;
+    cout << "z: ";
+    cin >> z;
+
+    for (int i = 0; i < (z * 60); i++)
+    {
+        k += (x + y);
+    }
+    cout << k;
+    return 0;
+}
+*/
+//12
+/*
+int main() {
+    setlocale(0, "");
+    int y, k = 0;
+    float x;
+    cout << "x: ";
+    cin >> x;
+    cout << "y: ";
+    cin >> y;
+
+    for (int i = 0; i < int(x * 10); i++)
+    {
+        k += y;
+    }
+    cout << k << " коп.";
+    return 0;
+}
+*/
+//6
+/*
+int main() {
+    setlocale(0, "");
+    float f, c, x;
+    cout << "Tf: ";
+    cin >> f;
+    x = 0.555556;
+    c = (f - 32) * x;
+
+    cout << c << " Tc";
+    return 0;
+}
+*/
+//7
+/*
+int main() {
+    setlocale(0, "");
+    float f, c, x;
+    cout << "Tc: ";
+    cin >> c;
+    x = 1.8;
+    f = (c * x) + 32;
+
+    cout << f << " Tf";
+    return 0;
+}
+*/
